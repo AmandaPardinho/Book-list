55512ca099e4aa5e562253483ad1f56f6699fd38
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-13 19:47:09 -0300

    Starting again

---
+++ display/booklist/java/BookList.java
@@ -0,0 +1,10 @@
+package display.booklist.java;
+
+import javax.swing.JOptionPane;
+
+public class BookList{
+    public static void main(String[]args){
+
+        JOptionPane.showInputDialog("What is the book name?");
+    }
+}
---
+++ out/production/Book-list/README.md
@@ -0,0 +1,7 @@
+# Book-list
+
+> Create a list of books and then select which one will be read by user
+---
+
+## Library
+> This project will simulate a library catalog and allow user interection.

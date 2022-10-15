d6aa3674f73b7bb74eb69215491745ff838722fa
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-14 22:12:04 -0300

    Unused code has been deleted

--- display/booklist/java/BookList.java
+++ display/booklist/java/BookList.java
@@ -7,11 +7,11 @@ public class BookList{
     public static void main(String[]args){
 
         Integer isbn;
-        //String user;
+        int answer;
         String bookName;
 
         Scanner scanner = new Scanner(System.in);
-
+        
         List<String>book = new ArrayList<>();
 
         JOptionPane.showMessageDialog(null, "Welcome to Pards' Library", "Pards' Library",
@@ -20,24 +20,7 @@ public class BookList{
         String user = JOptionPane.showInputDialog(null, "Enter a book name: ",
                 "Pards' Library", JOptionPane.QUESTION_MESSAGE);
 
-        if(user == null){
-            JOptionPane.showMessageDialog(null, "Please try again", "Pards' Library",
-                    JOptionPane.INFORMATION_MESSAGE);
-            JOptionPane.showInputDialog(null, "Enter a book name: ", "Pards' Library",
-                    JOptionPane.QUESTION_MESSAGE);
-        }else{
-            JOptionPane.showMessageDialog(null, "Book added successfully",
-                    "Pards' Library", JOptionPane.INFORMATION_MESSAGE);
-        }
-        //bookName = JOptionPane.getText(user);
 
-
-        /*JOptionPane.showMessageDialog(null, "Bem vindo ao Explorer",
-                "Explorer", JOptionPane.WARNING_MESSAGE);
-        String caminho = JOptionPane.showInputDialog(null,"Digite algum endereço.");
-
-        //while(!navega(caminho))
-          //  caminho = JOptionPane.showInputDialog(null,">>"+caminho+"\nDigite algum endereço.");*/
     }
 
 }

c5847df04ebcd43ef5c24ac9d3f8243aaa84cc59
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-14 23:54:42 -0300

    Created if/else

--- display/booklist/java/BookList.java
+++ display/booklist/java/BookList.java
@@ -11,7 +11,7 @@ public class BookList{
         String bookName;
 
         Scanner scanner = new Scanner(System.in);
-        
+
         List<String>book = new ArrayList<>();
 
         JOptionPane.showMessageDialog(null, "Welcome to Pards' Library", "Pards' Library",
@@ -20,7 +20,16 @@ public class BookList{
         String user = JOptionPane.showInputDialog(null, "Enter a book name: ",
                 "Pards' Library", JOptionPane.QUESTION_MESSAGE);
 
-
+        if((user != null) && (user.length() > 0)){
+            JOptionPane.showMessageDialog(null, "Book " + user + " was add successfully!",
+                    "Pards' Library", JOptionPane.INFORMATION_MESSAGE);
+        }else{
+            while(!(user.length() > 0)){
+                JOptionPane.showMessageDialog(null, "Please enter a book name.",
+                        "Pards' Library", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showInputDialog(null, "Enter a book name: ",
+                        "Pards' Library", JOptionPane.QUESTION_MESSAGE);
+            }
+        }
     }
-
 }

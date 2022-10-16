b2d94f99b07b15fcd8fe819c0a30e08c4ca2b91b
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-15 23:05:47 -0300

    Switch/case structure was created

--- display/booklist/java/BookList.java
+++ display/booklist/java/BookList.java
@@ -12,38 +12,34 @@ public class BookList{
 
     public static void setBook(String book){
         Integer i = -1;
+        String newBook;
 
         book = JOptionPane.showInputDialog(null, "Welcome to Pards' Library!\nPlease enter a book name:\n","Pards' Library",
                 JOptionPane.QUESTION_MESSAGE);
 
-        JOptionPane.showConfirmDialog(null, "Is " + book + " correct?", "Pards' Library",
-                JOptionPane.YES_NO_CANCEL_OPTION);
+        i = JOptionPane.showConfirmDialog(null, "Is " + book + " correct?", "Pards' Library",
+                JOptionPane.YES_NO_OPTION);
 
-        if(i == 0){
-            JOptionPane.showMessageDialog(null, book + " was add successfully!","Pards' " +
-                    "Library", JOptionPane.INFORMATION_MESSAGE);
-        }else{
-            while(i == 1){
-                JOptionPane.showMessageDialog(null, "Please enter the correct book name.",
-                        "Pards' Library", JOptionPane.INFORMATION_MESSAGE);
-                JOptionPane.showInputDialog(null, "Enter the correct book name: ",
-                        "Pards' Library", JOptionPane.QUESTION_MESSAGE);
+        //for(i = 0; i < 2; i++){
+            switch(i){
+                case 0:
+                    JOptionPane.showMessageDialog(null, book + " was add successfully!",
+                            "Pards' Library", JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Please enter the correct book name.",
+                                "Pards' Library", JOptionPane.INFORMATION_MESSAGE);
+                    newBook = JOptionPane.showInputDialog(null, "Enter the correct book name: ",
+                                "Pards' Library", JOptionPane.QUESTION_MESSAGE);
+                    JOptionPane.showConfirmDialog(null, "Is " + newBook + " correct?",
+                                "Pards' Library", JOptionPane.YES_NO_OPTION);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Option not allowed.", "Pards' " +
+                            "Library", JOptionPane.INFORMATION_MESSAGE);
             }
         }
-
-        if(i == 2){
-            JOptionPane.showConfirmDialog(null, "Would you like to add another book?",
-                    "Pards' Library", JOptionPane.YES_NO_OPTION);
-            if(i == 1){
-                JOptionPane.showInputDialog(null, "Enter a book name: ","Pards' Library",
-                        JOptionPane.QUESTION_MESSAGE);
-            }else{
-                JOptionPane.showMessageDialog(null, "Thank you for your effort. Have a nice day!",
-                        "Pards' Library", JOptionPane.INFORMATION_MESSAGE);
-            }
-        }
-
-    }
+    //}
     public static void main(String[]args){
        setBook(book);
 

ae62ae03f08e15fa9aa49ebfd2325e99e7d31c81
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-16 21:20:23 -0300

    Unused structures were deleted

--- display/booklist/java/BookList.java
+++ display/booklist/java/BookList.java
@@ -1,35 +1,37 @@
 package display.booklist.java;
 
 import javax.swing.JOptionPane;
+
 public class BookList{
     public static void setBook(){
-        String book;
-        String newBook;
+        String book1 = null;
+        String book2 = null;
+        String book3 = null;
         Integer answer1 = -1;
         Integer answer2 = -1;
         Integer answer3 = -1;
         Integer answer4 = -1;
 
-        book = JOptionPane.showInputDialog(null, "Welcome to Pards' Library!\nPlease enter a " +
+        book1 = JOptionPane.showInputDialog(null, "Welcome to Pards' Library!\nPlease enter a " +
             "book name:","Pards' Library", JOptionPane.QUESTION_MESSAGE);
-        answer1 = JOptionPane.showConfirmDialog(null, "Is " + book + " correct?", "Pards' " +
+        answer1 = JOptionPane.showConfirmDialog(null, "Is " + book1 + " correct?", "Pards' " +
             "Library", JOptionPane.YES_NO_OPTION);
 
         switch(answer1){
             case 0:
-                JOptionPane.showMessageDialog(null, book + " was add successfully!",
+                JOptionPane.showMessageDialog(null, book1 + " was add successfully!",
                     "Pards' Library", JOptionPane.INFORMATION_MESSAGE);
                 break;
             case 1:
                 JOptionPane.showMessageDialog(null, "Please enter the correct book name.",
                    "Pards' Library", JOptionPane.INFORMATION_MESSAGE);
-                newBook = JOptionPane.showInputDialog(null, "Enter the correct book name: ",
+                book2 = JOptionPane.showInputDialog(null, "Enter the correct book name: ",
                    "Pards' Library", JOptionPane.QUESTION_MESSAGE);
-                answer2 = JOptionPane.showConfirmDialog(null, "Is " + newBook + " correct?",
+                answer2 = JOptionPane.showConfirmDialog(null, "Is " + book2 + " correct?",
                    "Pards' Library", JOptionPane.YES_NO_OPTION);
                 switch(answer2){
                     case 0:
-                        JOptionPane.showMessageDialog(null, newBook + " was add " +
+                        JOptionPane.showMessageDialog(null, book2 + " was add " +
                             "successfully!","Pards' Library", JOptionPane.INFORMATION_MESSAGE);
                         break;
                     case 1:
@@ -38,21 +40,19 @@ public class BookList{
                         switch(answer3){
                             case 0:
                                 while(true){
-                                    book = JOptionPane.showInputDialog(null, "Welcome to " +
-                                        "Pards' Library!\nPlease enter a book name:","Pards' Library", JOptionPane.
-                                        QUESTION_MESSAGE);
+                                    book3 = JOptionPane.showInputDialog(null, "Welcome to " +
+                                        "Pards' Library!\nPlease enter another book name:","Pards' Library",
+                                        JOptionPane.QUESTION_MESSAGE);
                                     answer4 = JOptionPane.showConfirmDialog(null, "Is "
-                                        + book + "correct?", "Pards' Library", JOptionPane.YES_NO_OPTION);
+                                        + book3 + " correct?", "Pards' Library", JOptionPane.YES_NO_OPTION);
                                     if(answer4 == 0){
-                                        JOptionPane.showMessageDialog(null, book + " was add " +
+                                        JOptionPane.showMessageDialog(null, book3 + " was add " +
                                             "successfully!","Pards' Library", JOptionPane.INFORMATION_MESSAGE);
                                         break;
                                     }
                                 }
                                 break;
                             case 1:
-                                JOptionPane.showMessageDialog(null, "Thank you for your " +
-                                    "help!\nHave a nice day!","Pards' Library", JOptionPane.INFORMATION_MESSAGE);
                                 break;
                         }
                         break;
@@ -62,5 +62,7 @@ public class BookList{
 
     public static void main(String[]args){
         setBook();
+        JOptionPane.showMessageDialog(null, "Thank you for your help!\nHave a nice day!",
+            "Pards' Library", JOptionPane.INFORMATION_MESSAGE);
     }
 }
